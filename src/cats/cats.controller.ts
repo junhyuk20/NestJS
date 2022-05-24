a7ab05a4f40557b44084ad78caf39f4f0c83d547
junhyuk20 <kwk8002@naver.com>
2022-05-24 17:46:24 +0900

    cats모듈 생성및 컨트롤러 작성

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCat() {
+    return `all cat`;
+  }
+  //동적 라우팅
+  @Get(':id')
+  getOneCat() {
+    return `one cat`;
+  }
+
+  @Post()
+  createCat() {
+    return `create cat`;
+  }
+
+  //동적 라우팅
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+  //동적 라우팅
+  @Patch(':id')
+  updatePartialCat() {
+    return `update`;
+  }
+  //동적 라우팅
+  @Delete(':id')
+  deleteCat() {
+    return 'delete service';
+  }
+}
